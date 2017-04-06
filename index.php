c0eee04af09cb4b82a46f08d723a07bf4d55f47b
Daniel Choi <dhrchoi@gmail.com>
2017-04-06 11:50:50 -0700

    Initialize repository

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>To-Do App</title>
+	<script src="bower_components/jquery/dist/jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.css">
+	<script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+</head>
+<body>
+
+<?php 
+
+$title = "TaskMaker";
+
+
+ ?>
+
+<div class="container">
+	<h1><?php echo $title ?></h1>
+	<div class="jumbotron">
+		
+	</div>
+</div>
+
+</body>
+</html>
